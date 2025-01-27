7e87e4c9d7dbefc8116aaa09d72c589285810320
KFomin <fominkonstantiin@gmail.com>
2025-01-27 23:57:00 +0200

    add routes with styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {FavoritesComponent} from "./favorites/favorites.component";
+import {PhotoDetailComponent} from "./photo-detail/photo-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'photos/:id', component: PhotoDetailComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoDetailComponent,
+    FavoritesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
